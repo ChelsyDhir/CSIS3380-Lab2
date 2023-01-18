071205062793f8a87227eb143531887c246c8b6e
Chelsy <chelsydhir@gmail.com>
2023-01-17 22:17:53 -0800

    changes

--- js/script.js
+++ js/script.js
@@ -3,30 +3,18 @@ const button = document.getElementById("btn");
 const ul = document.getElementsByClassName("contact-list")[0];
 const totalContacts = contacts.length;
 
-displayAllContacts();
+displayContacts(0);
 pagination();
-// function getContacts() {
-//   for (var i = 0; i < contacts.length; i++) console.log(contacts[i]);
-//   return contacts;
-// }
 
-function displayAllContacts() {
-  //const image = document.getElementsByClassName("avatar")[0];
-  //const name = document.getElementsByTagName("h3")[1];
-  //const emailSpan = document.getElementsByTagName("span")[0];
-  //const joinedSpan = document.getElementsByTagName("span")[1];
-  // const details = document.getElementsByTagName('li')[0];
+function displayContacts(currentPage) {
   const title = document.getElementsByTagName("h3")[0];
   title.textContent = `Total: ` + contacts.length;
 
-  // name.innerHtml = "chelsy";
-  // console.log(contacts[0].name.textContent = "chelsy");
-  // console.log(contacts[0].joined);
-  // console.log(contacts[0].image);
-  // console.log(name.innerHtml);
-
   for (var i = 0; i < contacts.length; i++) {
-    let content = `<li class="contact-item cf">
+
+    if(i < currentPage*10 || i >= (currentPage+1)*10){
+        console.log("none");
+        var content = `<li class="contact-item cf">
                         <div class="contact-details">
                             <img class="avatar" src="${contacts[i].image}">
                             <h3>${contacts[i].name}</h3>
@@ -36,51 +24,35 @@ function displayAllContacts() {
                             <span class="date">Joined ${contacts[i].joined}</span>
                     </div>
                     </li>`;
-
-    ul.insertAdjacentHTML("beforeend", content);
-    //let li = document.createElement("li");
-    //li.setAttribute("class", "contact-item cf");
-
-    //image.src = contacts[i].image;
-    //name.textContent = contacts[i].name;
-    //emailSpan.textContent = contacts[i].email;
-    //joinedSpan.textContent = `Joined ` + contacts[i].joined;
-    //li = document.createElement("li");
-    // ul.appendChild(li);
+        
+        // ul.insertAdjacentHTML("beforeend", content);
+            // contacts[i].style.display = "none";
+        }else{
+            console.log("chelsy");
+            var content = `<li class="contact-item cf">
+                        <div class="contact-details">
+                            <img class="avatar" src="${contacts[i].image}">
+                            <h3>${contacts[i].name}</h3>
+                            <span class="email">${contacts[i].email}</span>
+                        </div>
+                        <div class="joined-details">
+                            <span class="date">Joined ${contacts[i].joined}</span>
+                    </div>
+                    </li>`;
+        ul.insertAdjacentHTML("beforeend", content);
+        
+            // contacts[i].style.display = "block";
+        }
   }
 
-  // contacts.forEach(contact => {
-  //     // image.innerHtml = contact.image;
-  //     name.textContent = contact.name;
-  //     const thisname = name.textContent;
-  //     // email.innerText = contact.email;
-  // });
-  // console.log(contacts[0].email);
 }
-
-// button.addEventListener("click", getContacts);
-
-function btnShow () {
-    var paginationUl = document.getElementById("pagination li");
-    //  Calculating the how many pages will be needed
-    var pages = Math.ceil(contentsNum / showContents); 
-    for (var i = 0; i < pages; i++) {
-      var li = document.createElement('li');
-      li.onclick = (function (j) { return function () {showPage(j);} }(i)); 
-  
-      // show the specific pages
-      var textnode = document.createTextNode(i + 1); 
-      li.appendChild(textnode);
-      paginationUl.appendChild(li);
-    }
-  }
-  
-
 function pagination() {
   const pageUl = document.getElementById("pagination li");
   const pages = Math.ceil(totalContacts / 10);
     for (var i = 0; i < pages; i++) {
-        let li = document.createElement('li');
+        let li = document.createElement("li");
+        li.setAttribute("class", 'pagination li');
+        li.onclick = (function (j) { return function () {displayAllContacts(j);} }(i)); 
         var textnode = document.createTextNode(i + 1);
         li.appendChild(textnode);
         pageUl.appendChild(li);
